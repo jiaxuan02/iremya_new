f071348db02c613bf3c85f24eff34888029f0589
frozt990 <129248305+frozt990@users.noreply.github.com>
2023-05-26 12:16:10 +0800

    Revert "player animation + script"
    This reverts commit 0975b7c3e8115d0867a5f7d9aa6a057731f46ab2.

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -32,12 +31,9 @@ public class PlayerMovement : MonoBehaviour
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
-            animator.SetBool("IsJumping", true);
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -52,11 +48,6 @@ public class PlayerMovement : MonoBehaviour
         {
             StartCoroutine(Dash());
         }
-    }
-
-    public void OnLanding()
-    {
-        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
