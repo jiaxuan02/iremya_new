94f9cdeacbee8d5efdbcd0791394c608953c779c
jiaxuan02 <131930550+jiaxuan02@users.noreply.github.com>
2023-07-30 11:40:57 +0800

    animation done, op(lvl1,2,4)

--- Assets/Script/Lvl4_WinTrigger1.cs
+++ Assets/Script/Lvl4_WinTrigger1.cs
@@ -9,16 +9,43 @@ public class Lvl4_WinTrigger1 : MonoBehaviour
     [Header("Visual Cue")]
     [SerializeField] private GameObject visualcue;
     [SerializeField] private GameObject winpanel;
+    [SerializeField] private GameObject stars1;
+    [SerializeField] private GameObject stars2;
+    [SerializeField] private GameObject stars3;
+
     private bool playerInRange;
+
+    private void Start()
+    {
+        // Initialize objects as needed in the Inspector
+        visualcue.SetActive(false);
+        winpanel.SetActive(false);
+        stars1.SetActive(false);
+        stars2.SetActive(false);
+        stars3.SetActive(false);
+    }
 
     private void Update() {
 
         if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying){
             visualcue.SetActive(true);
+
+            if (Lvl4_Score.scores == 3)
+            {
+                stars1.SetActive(true);
+            }
+            if (Lvl4_Score.scores == 6)
+            {
+                stars2.SetActive(true);
+            }
+            if (Lvl4_Score.lives == 2)
+            {
+                stars3.SetActive(true);
+            }
+
             if(Input.GetKeyDown(KeyCode.F))
             {
-                        winpanel.SetActive(true);
-       
+                winpanel.SetActive(true);
             }
             
         }
