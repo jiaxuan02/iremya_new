e885d821f118e01b3838a02ecc9dcaa04e1900fa
Cowfeeshop <dondoskrub@gmail.com>
2023-07-30 16:10:43 +0800

    Everything Fareid part done
    Level 1,2,3,4 all working

--- Assets/Script/NPCInteraction.cs
+++ Assets/Script/NPCInteraction.cs
@@ -2,80 +2,78 @@ using UnityEngine;
 
 public class NPCInteraction : MonoBehaviour
 {
-    public DialogueSystem dialogueSystem; // Reference to the DialogueSystem script on the NPC
-    public GameObject dialoguePanel; // Reference to the GameObject representing the dialogue panel
-    public GameObject interactionPrompt; // Reference to the GameObject representing the interaction prompt
+    public DialogueSystem dialogueSystem;
+    public GameObject dialoguePanel;
+    public GameObject interactionPrompt;
 
-    private bool playerInRange = false; // Flag to track if the player is in range for interaction
-    private bool dialogueTriggered = false; // Flag to track if the dialogue has been triggered
+    private bool playerInRange = false;
+    private bool dialogueTriggered = false;
+    private bool dialogueEnded = false; // New variable to track if dialogue has ended
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Check if the player's hitbox (tag: "Player") entered the NPC's trigger collider
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
-
-            // Show the interaction prompt
             interactionPrompt.SetActive(true);
+
+            // Reset the dialogueEnded flag when the player re-enters the NPC's hitbox
+            dialogueEnded = false;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        // Check if the player's hitbox (tag: "Player") exited the NPC's trigger collider
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-
-            // Hide the interaction prompt
             interactionPrompt.SetActive(false);
 
-            // Reset the dialogue trigger flag when the player moves away from the NPC
+            // Reset the dialogueTriggered flag when the player moves away from the NPC
             dialogueTriggered = false;
         }
     }
 
     private void Update()
     {
-        // Check if the player presses the "F" key to interact with the NPC
         if (playerInRange && Input.GetKeyDown(KeyCode.F) && !dialogueTriggered)
         {
-            // Disable the "F" key so it can't be used again to trigger the dialogue
             dialogueTriggered = true;
-
-            // Show the dialogue panel before starting the dialogue
             dialoguePanel.SetActive(true);
             dialogueSystem.ProgressDialogue();
 
-            // Check if the NPC has the "BadNPC" tag
             if (gameObject.CompareTag("BadNPC"))
             {
-                // Decrease the player's lives by 1
                 Lvl2_Score.lives--;
+                Lvl4_Score.lives--;
             }
             else
             {
                 // Start the dialogue with the NPC using the DialogueSystem
-             
             }
         }
 
-        // Check if the player presses the "E" key to progress the dialogue
-        if (Input.GetKeyDown(KeyCode.E) && dialogueTriggered)
+        // Allow interaction if dialogue has ended and player re-enters the NPC's hitbox
+        if (dialogueEnded && playerInRange && Input.GetKeyDown(KeyCode.F))
         {
-            // Continue the dialogue using the DialogueSystem
+            dialogueEnded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && dialogueTriggered && !dialogueEnded)
+        {
             dialogueSystem.ProgressDialogue();
+
+            // Check if the dialogue has ended
+            if (!dialoguePanel.activeSelf)
+            {
+                dialogueEnded = true;
+            }
         }
     }
 
-    // Method to be called from the DialogueSystem when the dialogue ends
     public void EndDialogue()
     {
-        // Hide the dialogue panel when the dialogue ends
         dialoguePanel.SetActive(false);
-
-        // Reset the dialogueTriggered flag to allow interaction with NPCs again
         dialogueTriggered = false;
     }
 }
